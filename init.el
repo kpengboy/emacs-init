8e7b77a172035d05dd829af8dc1cecd48866ae5b
Kevin Peng <kkpengboy@gmail.com>
2015-09-03 21:21:51 -0700

    Mark smart-tabs-mode as safe local variable

--- init.el
+++ init.el
@@ -54,3 +54,16 @@
 ; C/C++ customization
 (smart-tabs-insinuate 'c 'c++)
 (setq c-default-style "k&r")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((smart-tabs-mode)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
